6528b119d858b746693f1a61a0d2a6b73f8baf45
nerwinalamas <nerwinalamas@gmail.com>
2024-06-28 08:40:56 +0800

    refactor: auth store

--- frontend/src/store/useAuth.js
+++ frontend/src/store/useAuth.js
@@ -1,25 +1,9 @@
 import { create } from "zustand";
-// import { useCookies } from "react-cookie";
 
-const useAuthStore = create((set) => {
-    // const [cookies, setCookie, removeCookie] = useCookies(["token", "refreshToken"]);
-
-    return {
-        user: null,
-        isAuthenticated: false,
-
-        setUser: (user) => set((state) => ({ ...state, user })),
-        setIsAuthenticated: (isAuthenticated) => set({ isAuthenticated }),
-
-        // getAccessToken: () => cookies.token,
-        // getRefreshToken: () => cookies.refreshToken,
-
-        resetAuth: () => {
-            // removeCookie("token");
-            // removeCookie("refreshToken");
-            set({ user: null, isAuthenticated: false });
-        },
-    };
-});
+const useAuthStore = create((set) => ({
+    user: null,
+    login: (user) => set({ user }),
+    logout: () => set({ user: null }),
+}));
 
 export default useAuthStore;

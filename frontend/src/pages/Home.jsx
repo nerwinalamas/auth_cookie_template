cc24f8d135ffef58008bd3be1cd529116345eff2
nerwinalamas <nerwinalamas@gmail.com>
2024-06-28 08:42:15 +0800

    refactor: home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import { useLogoutUserMutation } from "../mutation/auth";
 import useAuthStore from "../store/useAuth";
 
 const Home = () => {
-    const { user, isAuthenticated } = useAuthStore();
+    const { user, logout } = useAuthStore((state) => ({
+        user: state.user,
+        logout: state.logout,
+    }));
+
+    const logoutUserMutation = useLogoutUserMutation();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await logoutUserMutation.mutateAsync();
+            logout();
+            navigate("/login");
+        } catch (error) {
+            console.error("Logout error:", error.message);
+        }
+    };
 
     return (
         <div>
-            {isAuthenticated ? (
-                <div>
-                    <h2>User Profile</h2>
-                    <p>
-                        Name: {user && user.firstName} {user && user.lastName}
-                    </p>
-                    <p>Email: {user && user.email}</p>
-                </div>
-            ) : (
-                <div>
-                    <p>User is not authenticated.</p>
-                </div>
-            )}
+            <h2>User Profile</h2>
+            <p>
+                Name: {user && user.firstName} {user && user.lastName}
+            </p>
+            <p>Email: {user && user.email}</p>
+            <button
+                onClick={handleLogout}
+                className="p-2 rounded-sm text-white bg-red-600"
+            >
+                Logout
+            </button>
         </div>
     );
 };
